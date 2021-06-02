c17358a2e4234e550b009df3aa273d7238e93bfa
DuanLeeWatkins <leeduan19@gmail.com>
2021-06-02 16:01:31 -0400

    updated files

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -7,6 +7,20 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 function reverseString(text) {
     // Code goes here
+    // return [...text].reduce((acc, char) => char + acc, '')
+    /* if (text === "") {
+        return ""
+    } else {
+        return reverseString(text.substr(1)) + text[0]
+    } */
+    /* let result = "";
+
+    for (let char of text) {
+        result = char + result
+    }
+    return result; */
+
+    return [...text].reverse().join('')
 }
 
 
